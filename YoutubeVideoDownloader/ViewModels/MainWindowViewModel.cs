83c2c9043e32b858cd22e004945df5a6ea83274f
haciy_yq41 <haciy_yq41@STHQ0127-02>
2023-07-14 17:51:33 +0400

    Add project files.

---
+++ YoutubeVideoDownloader/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeVideoDownloader.Commands;
+using YoutubeVideoDownloader.View.UCViews;
+
+namespace YoutubeVideoDownloader.ViewModels
+{
+    public class MainWindowViewModel:BaseViewModel
+    {
+        public RelayCommand ClickedCount { get; set; }
+        private string _url;
+
+        public string url
+        {
+            get { return _url; }
+            set { _url = value; OnPropertyChanged(); }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            ClickedCount = new RelayCommand((obj) =>
+            {
+                DownloadPercentUC downloadPercentUC = new DownloadPercentUC();
+                App.Progress.Children.Add(downloadPercentUC);
+               
+            });
+        }
+    }
+}
